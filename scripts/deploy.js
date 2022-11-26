0aba573d3bc9027af78e5d70a0722d785994c920
saxon <1@1.com>
2022-11-26 20:20:04 +0330

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,22 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const PolToken = await ethers.getContractFactory("PolyToken");
-  const poltoken = await PolToken.deploy();
+  // const PolToken = await ethers.getContractFactory("PolyToken");
+  // const poltoken = await PolToken.deploy();
 
-  await poltoken.deployed();
+  // await poltoken.deployed();
 
-  console.log(
-    `PolToken deployed to ${poltoken.address}`
-  );
+  // console.log(
+  //   `PolToken deployed to ${poltoken.address}`
+  // );
 
-  const TradeToken = await ethers.getContractFactory("TradeToken");
-  const tradetoken = await TradeToken.deploy();
-  await tradetoken.deployed();
+  // const TradeToken = await ethers.getContractFactory("TradeToken");
+  // const tradetoken = await TradeToken.deploy();
+  // await tradetoken.deployed();
 
-  console.log(
-    `TradeToken deployed to ${tradetoken.address}`
-  );
+  // console.log(
+  //   `TradeToken deployed to ${tradetoken.address}`
+  // );
 
   const Swapper = await ethers.getContractFactory("Swapper");
   const swapper = await Swapper.deploy();
@@ -37,4 +37,4 @@ main().catch((error) => {
 
 // 0xA0855bACF7712001DF3b3aa6209c879A323a97Ff Polytoken
 // 0xdB3457d1649eF0e3E306b8A7Ecd66E50d7Ef31cD Tradetoken
-// 0xEb22eDf3F11453AeC427e29308816CeD16b00a9A Wrapptoken
+// 0xb69660F4201F7bb18552A745E0fc00BE8075BE2d Wrapptoken
